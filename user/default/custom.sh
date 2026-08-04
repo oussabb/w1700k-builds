31531fc4f658e3ed5a46075db25d79ebe5874bd7
oussabb <65868967+oussabb@users.noreply.github.com>
2026-08-04 13:20:36 -0400

    Update custom.sh

--- user/default/custom.sh
+++ user/default/custom.sh
@@ -4,6 +4,4 @@ echo "Running custom commands"
 mv files/overview.js feeds/luci/applications/luci-app-attendedsysupgrade/htdocs/luci-static/resources/view/attendedsysupgrade/overview.js
 mkdir -p feeds/luci/modules/luci-mod-status/patches
 mv files/998-single-wiphy.patch feeds/luci/modules/luci-mod-status/patches/998-single-wiphy.patch
-mkdir -p package/feeds/packages/ovpn-dco/patches
-mv files/0002-tcp-version-guard.patch package/feeds/packages/ovpn-dco/patches/0002-tcp-version-guard.patch
 echo "=============================================="
